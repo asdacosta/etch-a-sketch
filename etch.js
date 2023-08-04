86f8c1931f787a9d09c72cd87e49fc5e1d223462
asdacosta <asdacosta18@gmail.com>
2023-08-04 00:46:05 -0700

    Add hover effect to leave a trail

--- etch.js
+++ etch.js
@@ -1,8 +1,17 @@
 const divArray = [];
 
-for (m = 0; m <= 256; m++) {
+for (m = 0; m < 256; m++) {
     const div = document.createElement('div');
     const container = document.querySelector('.container');
     container.appendChild(div);
     divArray.push(div);
 }
+
+divArray.forEach(div => {
+    div.addEventListener('mouseenter', () => {
+        div.style.backgroundColor = 'green';
+    })
+    div.addEventListener('mouseleave', () => {
+        div.style.backgroundColor = 'green';
+    })
+})
