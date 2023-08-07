b48b0a7dfac83cf49e633e7676ed57e24b5b3dc5
asdacosta <asdacosta18@gmail.com>
2023-08-07 13:14:35 -0700

    Create a function that creates a new grid each time a new value is set

--- etch.js
+++ etch.js
@@ -1,11 +1,32 @@
+let gridSize = 16;
 const divArray = [];
 
-for (m = 0; m < 256; m++) {
-    const div = document.createElement('div');
+function createGrid() {
     const container = document.querySelector('.container');
-    container.appendChild(div);
-    divArray.push(div);
+    container.innerHTML = '';
+
+    for (m = 0; m < gridSize * gridSize; m++) {
+        const div = document.createElement('div');
+        
+        container.appendChild(div);
+        divArray.push(div);
+    }
 }
+createGrid();
+
+const button = document.createElement('button');
+button.textContent = 'Set new Grid';
+button.style.border = '2px solid teal';
+button.addEventListener('click', () => {
+    newGridSize = parseInt(prompt('Enter the number of squares per side for a new grid: '));
+    if (newGridSize > 100 || isNaN(newGridSize)) {
+        alert('Please enter a value not more than 100');
+    } else {
+        gridSize = newGridSize;
+        createGrid();
+    }
+})
+document.body.prepend(button);
 
 divArray.forEach(div => {
     div.addEventListener('mouseenter', () => {
